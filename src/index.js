9d62fff1410814005dc79d87cd7b4a308b771393
Renato Marinho <renato.marinhofr@gmail.com>
2021-07-09 15:41:13 -0300

    deletar conta e ver o total da conta

--- src/index.js
+++ src/index.js
@@ -124,4 +124,20 @@ app.get("/account", verifyIfExistAccountCPF, (request, response) => {
   return response.json(customer);
 });
 
+app.delete("/account", verifyIfExistAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  return response.json(balance);
+});
+
 app.listen(3333);
