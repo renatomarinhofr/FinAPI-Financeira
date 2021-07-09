573b2c8ed86544bcb5bd6bfe74873ebb838f1375
Renato Marinho <renato.marinhofr@gmail.com>
2021-07-09 14:04:18 -0300

    listando extrato

--- src/index.js
+++ src/index.js
@@ -11,12 +11,11 @@ const customers = []
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
-  const custumerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
 
-  if (custumerAlreadyExists) {
-    return response.status(400).json({error: "Custumer alredy exists!"})
+  if (customerAlreadyExists) {
+    return response.status(400).json({error: "Customer already exists!"})
   }
-
   customers.push({
     cpf,
     name,
@@ -26,5 +25,15 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 
 });
+
+app.get("/statement/:cpf", (request, response) => {
+  const { cpf } = request.params;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  return response.json(customer.statement);
+})
+
+
 
 app.listen(3333);
